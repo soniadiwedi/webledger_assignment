20c84f30a824b82bafcf29e5461eb84447f9ff17
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-11-12 01:41:50 +0530

    Filter & Search Functionality Added

--- fronend/src/pages/home/Home.jsx
+++ fronend/src/pages/home/Home.jsx
@@ -1,73 +1,136 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Card from '../../components/card/cards/Card';
-import Search from '../../components/card/search/Search';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../../components/card/cards/Card";
+import Search from "../../components/card/search/Search";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
-  const [filter,setFilter] = useState("");
+  const [filter, setFilter] = useState("");
 
+  const cousines = [
+    "African",
+    "Asian",
+    "American",
+    "British",
+    "Cajun",
+    "Caribbean",
+    "Chinese",
+    "Eastern European",
+    "European",
+    "French",
+    "German",
+    "Greek",
+    "Indian",
+    "Irish",
+    "Italian",
+    "Japanese",
+    "Jewish",
+    "Korean",
+    "Latin American",
+    "Mediterranean",
+    "Mexican",
+    "Middle Eastern",
+    "Nordic",
+    "Southern",
+    "Spanish",
+    "Thai",
+    "Vietnamese",
+    "African",
+    "Asian",
+    "American",
+    "British",
+    "Cajun",
+    "Caribbean",
+    "Chinese",
+    "Eastern European",
+    "European",
+    "French",
+    "German",
+    "Greek",
+    "Indian",
+    "Irish",
+    "Italian",
+    "Japanese",
+    "Jewish",
+    "Korean",
+    "Latin American",
+    "Mediterranean",
+    "Mexican",
+    "Middle Eastern",
+    "Nordic",
+    "Southern",
+    "Spanish",
+    "Thai",
+    "Vietnamese",
+  ];
   useEffect(() => {
     getRecipeData();
-  }, [search]);
+  }, [search,filter]);
 
   const getRecipeData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      let res = await axios.get(`http://localhost:8080/api/food/find?search=${search}&filter=${filter}`);
+      let res = await axios.get(
+        `http://localhost:8080/api/food/find?search=${search}&filter=${filter}`
+      );
       setRecipes(res.data.data);
       setLoading(false);
     } catch (err) {
       setLoading(false);
-      console.log('error', err);
+      console.log("error", err);
     }
   };
 
-  const handleInput = (e) =>{
+  const handleInput = (e) => {
     const setData = setTimeout(() => {
-      setSearch(e.target.value)
+      setSearch(e.target.value);
     }, 3000);
-  }
-
+  };
 
   return (
-   <>
-   <div>
-    <Search search={search} setSearch={setSearch} handleInput={handleInput}/>
-    <select name="" id="">
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-      <option value=""></option>
-    </select>
-   </div>
-   <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px", width:"95%", margin:"auto" }}>
-      {loading ? (
-        <div className="loading-skeleton-card">
-          <h2 className="skeleton-title"></h2 >
-          <img className="skeleton-image"/>
-          <div className="skeleton-buttons">
-            <div className="skeleton-button"></div >
-            <div className="skeleton-button"></div >
+    <>
+      <div>
+        <Search
+          search={search}
+          setSearch={setSearch}
+          handleInput={handleInput}
+        />
+        <select name="" id="" onChange={(e)=>{setFilter(e.target.value)}}>
+          <option value="">Pls Select Your Favourite Cuisine.</option>
+          {cousines.map((item)=>{
+           return <option key={item} value={item}>{item}</option>
+          })}
+        </select>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "20px",
+          width: "95%",
+          margin: "auto",
+        }}
+      >
+        {loading ? (
+          <div className="loading-skeleton-card">
+            <h2 className="skeleton-title"></h2>
+            <img className="skeleton-image" />
+            <div className="skeleton-buttons">
+              <div className="skeleton-button"></div>
+              <div className="skeleton-button"></div>
+            </div>
           </div>
-        </div>
-      ) : (
-        recipes?.map((recipe) => (
-          <div key={recipe.id} >
-            <Card {...recipe} />
-          </div>
-        ))
+        ) : (
+          recipes?.map((recipe) => (
+            <div key={recipe.id}>
+              <Card {...recipe} />
+            </div>
+          ))
         )}
-    </div>
-        </>
+      </div>
+    </>
   );
 };
 
